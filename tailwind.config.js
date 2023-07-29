dd1ce74aa97d1b3e2ee12ec18b5d4b7532cc7e39
Josh Shavers <shaversj@gmail.com>
2023-07-29 14:48:12 -0500

    gh-pages changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       screens: {
-        'sm': '375px',
-        'lg': '1440px'
+        sm: "375px",
+        lg: "1440px",
       },
       colors: {
-        'red': "hsl(0, 78%, 62%)",
-        'cyan': "hsl(180, 62%, 55%)",
-        "orange": "hsl(34, 97%, 64%)",
-        "blue": "hsl(212, 86%, 64%)",
+        "my-red": "hsl(0, 78%, 62%)",
+        "my-cyan": "hsl(180, 62%, 55%)",
+        "my-orange": "hsl(34, 97%, 64%)",
+        "my-blue": "hsl(212, 86%, 64%)",
         "very-dark-blue": "hsl(234, 12%, 34%)",
         "grayish-blue": "hsl(229, 6%, 66%)",
         "very-light-gray": "hsl(0, 0%, 98%)",
       },
       fontFamily: {
-        'Poppins': ['Poppins', "sans-serif"]
-      }
+        Poppins: ["Poppins", "sans-serif"],
+      },
     },
   },
   plugins: [],
-}
+};
